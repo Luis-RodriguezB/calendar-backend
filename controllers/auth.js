143809263f3e0fccf4468671dba2a46708ee21a0
LuisER5 <luis.e.rodriguez@accenture.com>
2023-04-28 16:43:39 -0600

    ...

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async (req, res = response) => {
   const { email, password } = req.body;
@@ -21,31 +22,67 @@ const createUser = async (req, res = response) => {
 
     await user.save();
 
+    //Generate JWT
+    const token = await generateJWT(user.id, user.name);
+
     res.status(201).json({
       ok: true,
       msg: 'registro',
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token,
     });
 
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'Ups! Algo ha fallado, contacta con soporte.'
+      msg: 'Ups! Algo ha fallado, contacta con soporte'
     });
   }
 };
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo y la contraseña no coinciden'
+      });
+    }
+
+    // Confirmar password
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Contraseña incorrecta'
+      });
+    }
+
+    //Generate JWT
+    const token = await generateJWT(user.id, user.name);
+
+    res.json({
+      ok: true,
+      msg: 'login',
+      uid: user.id,
+      name: user.name,
+      token,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    });
+  }
 };
 
 const revalidateToken = (req, res = response) => {
